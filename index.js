69fa64be90dabc187b591f1cc6a71dc8a54af0be
Carlos <carlos.escobar_uk@live.com>
2020-11-19 20:19:55 +0000

    coins challenge
    cents to coins

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 
+// The function takes cents value (int) and needs to return the minimum number of coins combination of the same value.
+
+// The function should return an array where
+// 1 Penny === 1 cent
+// 1 Nickle === 5 cent
+// 1 dime === 10 cent
+// 1 quarter === 25 cent
+
+// coins[0] = pennies === 1 cent
+// coins[1] = nickles === 5 cent
+// coins[2] = dimes === 10 cent
+// coins[3] = quarters === 25 cent
+
+// So for example:
+// coinCombo(6) --> [1, 1, 0, 0]
+// coinCombo(3) --> [3, 0, 0, 0]
+// coinCombo(10) --> [0, 0, 0, 1]
+// coinCombo(14) --> [0, 0, 1, 4]
+
+const coinCombo = (cents) => {
+	const coins = [];
+
+	let quarters = Math.round(cents / 25);
+	cents = cents % 25;
+	let dimes = Math.round(cents / 10);
+	cents = cents % 10;
+	let nickels = Math.round(cents / 5);
+	cents = cents % 5;
+	let pennies = Math.round(cents / 1);
+
+	coins.push(pennies, nickels, dimes, quarters);
+
+	return coins;
+};
+
+console.log(coinCombo(30));
+
+let x = 5;
+let y = 10;
+
+console.log(x % y);
