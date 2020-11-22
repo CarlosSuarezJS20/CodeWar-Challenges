0f0768d4c243ce2a582b60d8bd7f0bda1b109ba2
Carlos <carlos.escobar_uk@live.com>
2020-11-22 19:02:44 +0000

    Level 7 exercise
    Dealing with Strings and numbers

--- index.js
+++ index.js
@@ -104,7 +104,7 @@ function accum(s) {
 	return string.substring(0, string.length - 1);
 }
 
-console.log(accum('abcdeU'));
+console.log(accum('qwertyuiopasdfghjklzxcvbnm'));
 
 // Best Practice:
 
@@ -142,3 +142,44 @@ console.log(disemvowel(string));
 // function disemvowel(str) {
 // 	return str.replace(/[aeiou]/gi, '');
 // }
+
+// Task 5
+// Now you have to write a function that takes an argument and returns the square of it.
+
+function squared(num) {
+	return num * num;
+
+console.log(squared(4));
+
+// A person's Life Path Number is calculated by adding each individual number in that person's date of birth, untill it is reduced to a single digit number.
+
+// For example, Albert Einstein's birthday is March 14, 1879. The calculation of his Life Path Number would look like this:
+
+// year: 1 + 8 + 7 + 9 = 25; 2 + 5 = 7
+// month: 0 + 3 = 3
+// day: 1 + 4 = 5
+// final result: 7 + 3 + 5 = 15; 1 + 5 = 6
+// Einstein's Life Path Number is therefore: 6
+
+// Write the function lifePathNumber(dateOfBirth) that accepts a date of birth (as a string) on the following format: "yyyy-mm-dd". Where "y" is year, "m" is month and "d" is day. The function shall return a one digit integer between 1 and 9 which represents the Life Path Number of the given date of birth.
+
+// You do not need to check that the input to the lifePathNumber()-function is correct format. You can assume that the input to the function will always be a valid date (as a string) with the format: "yyyy-mm-dd".
+
+// Note: If the month or day is a single digit number, then it shall be preceeded by a 0 (zero). For example, in Einstein's case the month of March is 3; which is a single digit number. The function shall in this case be called with the following parameter: lifePathNumber("1879-03-14").
+
+function lifePathNumber(dateOfBirth) {
+	let lifenum = dateOfBirth.replace(/-/g, '');
+	while (lifenum.length > 1) {
+		let lifenumArray = lifenum.split('').map((c) => +c);
+		console.log(lifenumArray);
+		lifenum = lifenumArray.reduce((c, p) => c + p).toString();
+	}
+
+	return +lifenum;
+}
+
+console.log(
+	lifePathNumber(
+		'999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999'
+	)
+);
