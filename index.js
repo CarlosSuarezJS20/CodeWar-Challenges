fcb2b6407e128ae6afcedc42ebf9ac85583137d6
Carlos <carlos.escobar_uk@live.com>
2020-11-30 21:15:25 +0000

    Task 10 from CodeWars
    CodeWars exercise of the day

--- index.js
+++ index.js
@@ -281,3 +281,48 @@ function findScreenHeight(width, ratio) {
 //   const height = width / widthRatio * heightRatio
 //   return width + 'x' + height
 // }
+
+// Task 10
+
+// Scenario
+
+// Now that the competition gets tough it will Sort out the men from the boys .
+// Men are the Even numbers and Boys are the odd
+
+// Task
+// Given an array/list [] of n integers , Separate The even numbers from the odds , or Separate the men from the boys
+
+// Notes
+// Return an array/list where Even numbers come first then odds
+
+// Since , Men are stronger than Boys , Then Even numbers in ascending order While odds in descending .
+
+// Array/list size is at least *4*** .
+
+// Array/list numbers could be a mixture of positives , negatives .
+
+// Have no fear , It is guaranteed that no Zeroes will exists .
+
+function menFromBoys(arr) {
+	const men = [];
+	const boys = [];
+	let line = [];
+
+	const menAndBoys = arr.filter((person, index) => {
+		return arr.indexOf(person) === index;
+	});
+
+	for (let person of menAndBoys) {
+		if (person % 2 === 0) {
+			men.push(person);
+		} else {
+			boys.push(person);
+		}
+	}
+
+	line = [...men.sort((a, b) => a - b), ...boys.sort((a, b) => b - a)];
+
+	return line;
+}
+
+console.log(menFromBoys([82, 91, 72, 76, 76, 100, 8]));
